54d57fc16e5fc16f14334ee5178efada84b937c7
Alireza Mousavi <moosavi78@gmail.com>
2021-04-23 12:32:20 +0430

    Separate main module and command handler module

--- command_handler.py
+++ command_handler.py
@@ -1,25 +1,12 @@
-from flask import Flask, request
-from flask.json import jsonify
+from flask import Blueprint, request
 from sqlalchemy.exc import IntegrityError
 from http import HTTPStatus
 from models import *
 from event_handler import bus
 
-app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_BINDS'] = {
-	'transactions': 'sqlite:///transactions.sqlite3',
-	'books': 'sqlite:///books.sqlite3'
-}
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False	# To silence a warning
+command_handler = Blueprint('Command Handler', __name__)
 
-@app.before_first_request
-def setup_db():
-	db.init_app(app)
-	db.create_all()
-
-
-@app.route('/books/create', methods=['POST'])
+@command_handler.route('/books/create', methods=['POST'])
 def create_book():
 	data = request.json
 	# TODO: Needs better checking
@@ -41,7 +28,7 @@ def create_book():
 		return {'message': 'Bad Request'}, HTTPStatus.BAD_REQUEST
 
 
-@app.route('/books/update/<int:book_id>', methods=['PUT'])
+@command_handler.route('/books/update/<int:book_id>', methods=['PUT'])
 def update_book(book_id):
 	data = request.json
 	# TODO: Needs better checking
@@ -64,7 +51,7 @@ def update_book(book_id):
 		return {'message': 'Bad Request'}, HTTPStatus.BAD_REQUEST
 
 
-@app.route('/books/delete/<int:book_id>', methods=['PUT'])
+@command_handler.route('/books/delete/<int:book_id>', methods=['PUT'])
 def delete_book(book_id):
 	data = request.json
 	# TODO: Needs better checking
@@ -82,12 +69,3 @@ def delete_book(book_id):
 		return {'message': 'Ok'}, HTTPStatus.OK
 	except IntegrityError:
 		return {'message': 'Bad Request'}, HTTPStatus.BAD_REQUEST
-
-
-@app.route('/books/log', methods=['GET'])
-def show_transactions():
-	return jsonify([tr.to_dict() for tr in Transaction.query.all()])
-
-
-if __name__ == '__main__':
-	app.run()
---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+from flask.json import jsonify
+from models import *
+from command_handler import command_handler
+
+
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.register_blueprint(command_handler)
+app.config['SQLALCHEMY_BINDS'] = {
+	'transactions': 'sqlite:///transactions.sqlite3',
+	'books': 'sqlite:///books.sqlite3'
+}
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False	# To silence a warning
+
+@app.before_first_request
+def setup_db():
+	db.init_app(app)
+	db.create_all()
+
+@app.route('/books/log', methods=['GET'])
+def show_transactions():
+	return jsonify([tr.to_dict() for tr in Transaction.query.all()])
+
+
+if __name__ == '__main__':
+	app.run()
